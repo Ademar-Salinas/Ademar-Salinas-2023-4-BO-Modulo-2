11e00189151e1c0a33b66422773c8de13360e1a6
Ademar Maximiliano Salinas Acero <adelord1234@gmail.com>
2023-05-31 18:00:39 -0400

    Second-Class-Homework

--- game/components/enemies/enemy.py
+++ game/components/enemies/enemy.py
@@ -3,8 +3,7 @@ import random
 
 from pygame.sprite import Sprite
 
-from game.utils.constants import ENEMY_1, SCREEN_HEIGHT, SCREEN_WIDTH
-
+from game.utils.constants import ENEMY_1, ENEMY_2, SCREEN_HEIGHT, SCREEN_WIDTH
 
 
 class Enemy(Sprite):
@@ -15,20 +14,27 @@ class Enemy(Sprite):
     MOV_X = {0: 'left', 1: 'right' }
     SHIP_WIDTH = 40
     SHIP_HEIGHT = 60
-
+    n_enemy = 0
 
     def __init__(self):
-        self.image = ENEMY_1
+        if (self.n_enemy == 0):
+            self.image = ENEMY_1
+            self.speed_y = self.SPEED_Y
+
+        elif (self.n_enemy == 1):
+            self.image = ENEMY_2
+            self.speed_y = random.choice((5, 6, 7)) 
         self.image = pygame.transform.scale(self.image, (40, 60))
         self.rect = self.image.get_rect()
         self.rect.x = self.X_POS_LIST[random.randint(0, 10)]
         self.rect.y = self.Y_POS
-        self.speed_y = self.SPEED_Y
         self.speed_x = self.SPEED_X
         self.movement_x = self.MOV_X[random.randint(0, 1)]
         self.move_x_for = random.randint(30, 100)
         self.index = 0
-
+            
+       
+            
     def update(self, ships):
         self.rect.y += self.speed_y
 
@@ -53,3 +59,4 @@ class Enemy(Sprite):
         elif (self.index >= self.move_x_for and self.movement_x == 'left') or (self.rect.x <= 10):
             self.movement_x = 'right'
             self.index = 0
+
--- game/components/enemies/enemy_manager.py
+++ game/components/enemies/enemy_manager.py
@@ -3,7 +3,7 @@ from game.components.enemies.enemy import Enemy
 class EnemyManager:
     def __init__(self):
         self.enemies = []
-
+        self.enemy = Enemy
     def update(self):
         self.add_enemy()
 
@@ -15,6 +15,13 @@ class EnemyManager:
             enemy.draw(screen)
 
     def add_enemy(self):
-        if len(self.enemies) < 1:
+        if len(self.enemies) < 2:
             enemy = Enemy()
             self.enemies.append(enemy)
+            if len(self.enemies) == 0:
+                self.enemy.n_enemy = 0
+            elif len(self.enemies) == 1:
+                self.enemy.n_enemy = 1
+    
+            
+            
